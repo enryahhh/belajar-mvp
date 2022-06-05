52986f9c46d18ae4767eb9c04e2bfa418460b4eb
Enryahhh <linggaj28@gmail.com>
2022-06-05 14:37:18 +0700

    coba bottom navigation

---
+++ app/src/main/java/com/example/belajarmvp1/helper/CloseDialogListener.java
@@ -0,0 +1,7 @@
+package com.example.belajarmvp1.helper;
+
+import android.content.DialogInterface;
+
+public interface CloseDialogListener {
+    public void handleDialogCloseList(DialogInterface dialog);
+}
